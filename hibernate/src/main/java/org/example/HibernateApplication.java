4bdb2ad0fdfec1bea2bae486a17800af12e96213
Krolb <pivamaks@gmail.com>
2025-05-31 00:00:13 +0300

    добавлена транзакционность, и логирование ошибок

--- hibernate/src/main/java/org/example/HibernateApplication.java
+++ hibernate/src/main/java/org/example/HibernateApplication.java
@@ -1,21 +1,20 @@
 package org.example;
 
-import org.example.config.ExceptionHandler;
+import lombok.extern.slf4j.Slf4j;
 import org.example.config.HibernateUtils;
 import org.example.entity.User;
 import org.example.repository.impl.UserDAOImpl;
-import org.hibernate.Session;
 
 import java.util.Scanner;
 
+@Slf4j
 public class HibernateApplication {
 
     private static final Scanner input = new Scanner(System.in);
 
     public static void main(String[] args) {
-        Session session = HibernateUtils.getSessionFactory().openSession();
 
-        UserDAOImpl rep = new UserDAOImpl(session);
+        UserDAOImpl rep = new UserDAOImpl(HibernateUtils.getSessionFactory());
 
         while (true) {
             System.out.print("""
@@ -28,44 +27,60 @@ public class HibernateApplication {
                     Выберите действие:""");
 
             switch (input.next()) {
-                case "1" -> ExceptionHandler.runSafe(() -> System.out.println(rep.findAll()));
-                case "2" -> ExceptionHandler.runSafe(() -> {
-                    User user = new User();
-                    System.out.print("Введите имя:");
-                    user.setName(input.next());
-                    System.out.print("Введите email:");
-                    user.setEmail(input.next());
-                    System.out.print("Введите возраст:");
-                    user.setAge(Integer.parseInt(input.next()));
-                    rep.save(user);
-                    System.out.println("Пользователь был сохранен");
-                });
-                case "3" -> ExceptionHandler.runSafe(() -> {
-                    System.out.print("Введите id пользователя для изменения данных:");
-                    User user = rep.findById(Long.parseLong(input.next()));
-                    System.out.println("""
-                            1 - имя
-                            2 - email
-                            3 - возраст
-                            Выберете параметр для изменения:""");
-                    switch (input.next()) {
-                        case "1" -> user.setName(input.next());
-                        case "2" -> user.setEmail(input.next());
-                        case "3" -> user.setAge(Integer.parseInt(input.next()));
-                        default -> System.out.println("Команда не распознан");
+                case "1" -> System.out.println(rep.findAll());
+                case "2" -> {
+                    try {
+                        User user = new User();
+                        System.out.print("Введите имя:");
+                        user.setName(input.next());
+                        System.out.print("Введите email:");
+                        user.setEmail(input.next());
+                        System.out.print("Введите возраст:");
+                        user.setAge(Integer.parseInt(input.next()));
+                        rep.save(user);
+                        System.out.println("Пользователь был сохранен");
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
                     }
-                    rep.update(user);
-                    System.out.println("Данные пользователя обновлены");
-                });
-                case "4" -> ExceptionHandler.runSafe(() -> {
-                    System.out.print("Введите id пользователя:");
-                    rep.delete(Long.parseLong(input.next()));
-                    System.out.println("Пользователь успешно удален");
-                });
-                case "5" -> ExceptionHandler.runSafe(() -> {
-                    System.out.print("Введите id пользователя:");
-                    System.out.println(rep.findById(Long.parseLong(input.next())));
-                });
+                }
+                case "3" -> {
+                    try {
+                        System.out.print("Введите id пользователя для изменения данных:");
+                        User user = rep.findById(Long.parseLong(input.next()));
+                        System.out.println("""
+                                1 - имя
+                                2 - email
+                                3 - возраст
+                                Выберете параметр для изменения:""");
+                        switch (input.next()) {
+                            case "1" -> user.setName(input.next());
+                            case "2" -> user.setEmail(input.next());
+                            case "3" -> user.setAge(Integer.parseInt(input.next()));
+                            default -> System.out.println("Команда не распознан");
+                        }
+                        rep.update(user);
+//                        System.out.println("Данные пользователя обновлены");
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    }
+                }
+                case "4" -> {
+                    try {
+                        System.out.print("Введите id пользователя:");
+                        rep.delete(Long.parseLong(input.next()));
+//                        System.out.println("Пользователь успешно удален");
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    }
+                }
+                case "5" -> {
+                    try {
+                        System.out.print("Введите id пользователя:");
+                        System.out.println(rep.findById(Long.parseLong(input.next())));
+                    } catch (Exception e) {
+                        log.error(e.getMessage());
+                    }
+                }
                 case "0" -> {
                     System.out.println("Сессия завершена");
                     HibernateUtils.shutdown();
--- hibernate/src/main/java/org/example/repository/impl/UserDAOImpl.java
+++ hibernate/src/main/java/org/example/repository/impl/UserDAOImpl.java
@@ -1,48 +1,141 @@
 package org.example.repository.impl;
 
+import jakarta.persistence.PersistenceException;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.example.entity.User;
 import org.example.repository.UserDAO;
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.exception.ConstraintViolationException;
 
+import java.sql.SQLException;
+import java.util.Collections;
 import java.util.List;
 
 @Slf4j
 @RequiredArgsConstructor
 public class UserDAOImpl implements UserDAO {
 
-    private final Session session;
+    private final SessionFactory sessionFactory;
 
     @Override
     public User findById(Long id) {
-        return this.session.get(User.class, id);
+        Transaction tx;
+        User user = null;
+        try (Session session = sessionFactory.openSession()) {
+            tx = session.beginTransaction();
+            user = session.get(User.class, id);
+            tx.commit();
+        } catch (Exception e) {
+            log.error("Failed to find user with id {}", id);
+        }
+        return user;
     }
 
     @Override
     public void save(User user) {
-        this.session.beginTransaction();
-        this.session.persist(user);
-        this.session.getTransaction().commit();
-        log.info("User saved: {}", user.toString());
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            session.persist(user);
+            tx.commit();
+            log.info("User saved: {}", user);
+        } catch (HibernateException e) {
+            log.error("Hibernate error: {}", e.getMessage());
+        } catch (PersistenceException e) {
+            handleConstraintViolation(e);
+        } catch (Exception e) {
+            log.error("Unexpected error");
+        } finally {
+            rollbackIsActive(tx);
+            closeSession(session);
+        }
     }
 
     @Override
     public void update(User user) {
-        this.session.merge(user);
-        log.info("User update: {}", user.toString());
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            session.merge(user);
+            tx.commit();
+            log.info("User updated: {}", user);
+        } catch (ConstraintViolationException e) {
+            handleConstraintViolation(e);
+        } catch (HibernateException e) {
+            log.error("Hibernate error on update: {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Unexpected error on update: {}", e.getMessage());
+        } finally {
+            rollbackIsActive(tx);
+            closeSession(session);
+        }
     }
 
     @Override
     public void delete(Long id) {
-        this.session.beginTransaction();
-        this.session.remove(this.findById(id));
-        this.session.getTransaction().commit();
-        log.info("User with id {} was deleted", id);
+        Session session = null;
+        Transaction tx = null;
+        try {
+            session = sessionFactory.openSession();
+            tx = session.beginTransaction();
+            User user = session.get(User.class, id);
+            session.remove(user);
+            tx.commit();
+        } catch (ConstraintViolationException e) {
+            handleConstraintViolation(e);
+        } catch (HibernateException e) {
+            log.error("Hibernate error on delete: {}", e.getMessage());
+        } catch (Exception e) {
+            log.error("Unexpected error on delete: {}", e.getMessage());
+        } finally {
+            rollbackIsActive(tx);
+            closeSession(session);
+        }
     }
 
     @Override
     public List<User> findAll() {
-        return this.session.createQuery("from User", User.class).getResultList();
+        try (Session session = sessionFactory.openSession()) {
+            return session.createQuery("from User", User.class).list();
+        } catch (Exception e) {
+            log.error("Failed to fetch users");
+            return Collections.emptyList();
+        }
+    }
+
+    private void rollbackIsActive(Transaction tx) {
+        if (tx != null && tx.isActive()) try {
+            tx.rollback();
+        } catch (Exception e) {
+            log.error("Failed to rollback");
+        }
+    }
+
+    private void closeSession(Session session) {
+        if (session != null && session.isOpen()) session.close();
+    }
+
+    private void handleConstraintViolation(PersistenceException e) { //fixme: переделать на sout, изза SQLExceptionHelper
+        SQLException sqlEx = (SQLException) e.getCause().getCause();
+        String sqlState = sqlEx.getSQLState();
+
+        switch (sqlState) {
+            case "23505":
+                log.error("Unique constraint violated: {}", sqlEx.getMessage());
+                break;
+            case "23502":
+                log.error("Not-null constraint violated: {}", sqlEx.getMessage());
+                break;
+            default:
+                log.error("Constraint violation: {}", sqlEx.getMessage());
+        }
     }
 }
