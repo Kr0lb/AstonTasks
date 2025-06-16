bd0243dfee712b32a0ce9f723d9a9987b70cfdbd
Krolb <pivamaks@gmail.com>
2025-06-16 14:21:07 +0300

    переименован ExceptionController.java на ExceptionHandler

--- spring/src/main/java/org/example/exception/ExceptionHandler.java
+++ spring/src/main/java/org/example/exception/ExceptionHandler.java
@@ -1,18 +1,17 @@
-package org.example.controller;
+package org.example.exception;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @ControllerAdvice
-public class ExceptionController {
+public class ExceptionHandler {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler({Exception.class})
+    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
     public String exceptionHandler(Exception e) {
         log.error("{}: {}", e.getClass().getName(), e.getMessage());
         return e.getMessage() != null ? e.getMessage() : e.getLocalizedMessage();
