7adb1170d77c9cf74898d267716b21f85b1c7a3f
Krolb <pivamaks@gmail.com>
2025-06-18 18:28:28 +0300

    main для notification-service

---
+++ notifdication-service/src/main/java/org/example/NotificationServiceMain.java
@@ -0,0 +1,11 @@
+package org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NotificationServiceMain {
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceMain.class, args);
+    }
+}
