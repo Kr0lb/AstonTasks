3c8e1c180f9d20a9aa77c0f7bb68e895e9f2fe6c
Krolb <pivamaks@gmail.com>
2025-06-18 16:51:43 +0300

    переимнован ExceptionHandler.java

--- spring/src/main/java/org/example/exception/ExceptionHandlerImpl.java
+++ spring/src/main/java/org/example/exception/ExceptionHandlerImpl.java
@@ -3,15 +3,16 @@ package org.example.exception;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @ControllerAdvice
-public class ExceptionHandler {
+public class ExceptionHandlerImpl {
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @org.springframework.web.bind.annotation.ExceptionHandler({Exception.class})
+    @ExceptionHandler({Exception.class})
     public String exceptionHandler(Exception e) {
         log.error("{}: {}", e.getClass().getName(), e.getMessage());
         return e.getMessage() != null ? e.getMessage() : e.getLocalizedMessage();
