27d023fdf99fc9813caddd8b8161450ea7492eaa
Krolb <pivamaks@gmail.com>
2025-06-18 17:52:15 +0300

    инитиализация notification-service, добавлена kafka

---
+++ notifdication-service/src/main/java/org/example/service/KafkaConsumerService.java
@@ -0,0 +1,16 @@
+package org.example.service;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class KafkaConsumerService {
+    private final EmailService emailService;
+
+    @KafkaListener(topics = "user-create", groupId = "notification-group")
+    public void listenUserCreate(String message) {
+        System.err.println(message);
+    }
+}
