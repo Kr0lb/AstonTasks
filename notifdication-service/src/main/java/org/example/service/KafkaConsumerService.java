4ab3f92cc93c2e1fea8fb796d5700b7a21c4a3a2
Krolb <pivamaks@gmail.com>
2025-06-19 18:58:38 +0300

    разделенпие topic удаление и создание

--- notifdication-service/src/main/java/org/example/service/KafkaConsumerService.java
+++ notifdication-service/src/main/java/org/example/service/KafkaConsumerService.java
@@ -10,8 +10,14 @@ public class KafkaConsumerService {
     private final EmailService emailService;
 
     @KafkaListener(topics = "user-create", groupId = "notification-group")
-    public void listenUserCreate(String message) {
-        emailService.sendEmail("pivamaks@gmail.com", "subject", message);
+    public void listenUserCreate(String email) {
+        emailService.sendEmail(email, "Create", "Здравствуйте! Ваш аккаунт на сайте forum был успешно создан.");
+        System.err.println("create");
+    }
+
+    @KafkaListener(topics = "user-delete", groupId = "notification-group")
+    public void listenUserDelete(String email) {
+        emailService.sendEmail(email, "Delete", "Здравствуйте! Ваш аккаунт был удалён.");
         System.err.println("create");
     }
 }
