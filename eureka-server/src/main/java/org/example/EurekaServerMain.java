e7d38b002952c9ca3d6db830aa768df97514ea83
Krolb <pivamaks@gmail.com>
2025-07-02 21:49:51 +0300

    добавлена eureka

--- eureka-server/src/main/java/org/example/EurekaServerMain.java
+++ eureka-server/src/main/java/org/example/EurekaServerMain.java
@@ -2,10 +2,12 @@ package org.example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-public class SpringMain {
+@EnableEurekaServer
+public class EurekaServerMain {
     public static void main(String[] args) {
-        SpringApplication.run(SpringMain.class, args);
+        SpringApplication.run(EurekaServerMain.class, args);
     }
 }
